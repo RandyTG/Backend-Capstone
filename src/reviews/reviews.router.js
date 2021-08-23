3a7ad375968e416d2cb74294a822ca453265d693
Randy Tavarez <randy_2787@hotmail.com>
2021-08-23 12:53:23 -0400

    cors

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
 
-router.route("/:reviewId").delete(controller.delete).all(methodNotAllowed);
+router
+  .route("/:reviewId")
+  .all(cors())
+  .delete(controller.delete)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
 module.exports = router;
--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/").get(cors(), controller.list).all(methodNotAllowed);
 
 module.exports = router;
