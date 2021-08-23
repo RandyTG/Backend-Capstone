e731257e15f4a6c50fdc72c2f12558d9795472d9
Randy Tavarez <randy_2787@hotmail.com>
2021-08-23 02:12:21 -0400

    reviews controller and service files complete

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -12,11 +12,25 @@ async function reviewExists(req, res, next) {
   return next({ status: 404, message: `Review cannot be found.` });
 }
 
+async function update(req, res) {
+  const { reviewId } = req.params;
+
+  const updatedReview = {
+    ...req.body.data,
+    review_id: reviewId,
+  };
+  await service.update(updatedReview);
+
+  const data = await service.updateRead(reviewId);
+  res.json({ data });
+}
+
 async function destroy(req, res) {
   await service.delete(res.locals.review.review_id);
   res.sendStatus(204);
 }
 
 module.exports = {
+  update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
   delete: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
 };
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,7 +1,30 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
+
+const addCritic = mapProperties({
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+});
 
 function read(reviewId) {
   return knex("reviews").select("*").where({ review_id: reviewId }).first();
+}
+
+function update(updatedReview) {
+  return knex("reviews")
+    .select("*")
+    .where({ review_id: updatedReview.review_id })
+    .update(updatedReview, "*");
+}
+
+function updateRead(reviewId) {
+  return knex("reviews as r")
+    .join("critics as c", "r.critic_id", "c.critic_id")
+    .select("r.*", "c.*")
+    .where({ "r.review_id": reviewId })
+    .then(addCritic)
+    .then((record) => record[0]);
 }
 
 function destroy(reviewId) {
@@ -11,4 +34,6 @@ function destroy(reviewId) {
 module.exports = {
   delete: destroy,
   read,
+  update,
+  updateRead,
 };
