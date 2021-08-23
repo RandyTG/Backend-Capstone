9d304f4465423a3e6a2e6cc2a96f80398a10cf8d
Randy Tavarez <randy_2787@hotmail.com>
2021-08-23 13:31:13 -0400

    cors

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -3,6 +3,6 @@ const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 const cors = require("cors");
 
-router.route("/").get(cors(), controller.list).all(methodNotAllowed);
+router.route("/").all(cors()).get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
