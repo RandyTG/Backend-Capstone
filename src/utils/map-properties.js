734c2e98acac64a7e0dd6f980bd805b14ebe615d
Randy Tavarez <randy_2787@hotmail.com>
2021-08-23 12:35:49 -0400

    updated map function

--- src/utils/map-properties.js
+++ src/utils/map-properties.js
@@ -10,11 +10,9 @@ const lodash = require("lodash");
 function mapProperties(configuration) {
   return (data) => {
     if (data) {
-      return data.map((data) => {
-        return Object.entries(data).reduce((accumulator, [key, value]) => {
-          return lodash.set(accumulator, configuration[key] || key, value);
-        }, {});
-      });
+      return Object.entries(data).reduce((accumulator, [key, value]) => {
+        return lodash.set(accumulator, configuration[key] || key, value);
+      }, {});
     }
     return data;
   };
